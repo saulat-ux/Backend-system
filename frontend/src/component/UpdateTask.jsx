f822e29213a278f74b0b0a2c7be989335ad98e94
saulat-ux <saulat.zubair@gmail.com>
2023-09-20 20:17:23 +0500

    completed the update operation

--- frontend/src/component/UpdateTask.jsx
+++ frontend/src/component/UpdateTask.jsx
@@ -2,37 +2,26 @@ import React, { useState } from 'react'
 import axios from 'axios';
 import { useSearchParams } from 'react-router-dom';
 
-
-
-
-
-
-
 const UpdateTask = () => {
   const [seacrhParams] = useSearchParams()
+  const [data , setData] = useState([])
   const dataID = seacrhParams.get('dataID')
   console.log('the id from url is '+ dataID)
     const [name ,setName] = useState('')
     const [price ,setPrice] = useState('')
 
     const handleSubmit = async (e) => {
-        e.preventDefault()
-        console.log(name , price)
-                   
+        e.preventDefault()         
         try {
-               const updatedData =  await axios.patch(`http://localhost:3000/api/v1/properties/${dataID}`)
-               console.log( `the update data is  ${updatedData.data.property.name}`)
-               updatedData.data.property.name = name
-               updatedData.data.property.price = price
-               console.log( updatedData.data.property.name, updatedData.data.property.price)
-            
+              await axios.patch(`http://localhost:3000/api/v1/properties/${dataID}` , {
+                  name:name,
+                  price:price,
+                })
+              
         } catch (error) {
             console.log('could not update '+ error)
         }
-   
-    
     }
-
   return (
     <form onSubmit={handleSubmit}>
       <label><span>name</span>
@@ -44,9 +33,7 @@ const UpdateTask = () => {
         <input type="text" name='price' onChange={(e)=> setPrice(e.target.value) } />
         </label>
                 <button >submit</button>
-
     </form>
   )
 }
-
 export default UpdateTask
